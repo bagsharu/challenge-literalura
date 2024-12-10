1c30c0379df351b688b7642e188efae018cc2c82
bagsharu <gabs.haru@gmail.com>
2024-12-10 18:14:21 -0300

    Mapeando atributos do Record

--- src/main/java/bagsharu/literalura/model/LivroInfo.java
+++ src/main/java/bagsharu/literalura/model/LivroInfo.java
@@ -2,7 +2,21 @@ package bagsharu.literalura.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record LivroInfo(@JsonAlias("count") Integer countLivro) {
+public record LivroInfo(@JsonAlias("title") String tituloLivro,
+                        @JsonAlias("languages") List<String> linguagemLivro,
+                        @JsonAlias("authors") List<AutorInfo> autorLivro,
+                        @JsonAlias("download_count") Integer downloadTotal) {
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+record AutorInfo(@JsonAlias("name") String nomeAutor,
+                        @JsonAlias("birth_year") LocalDate anoNascimento,
+                        @JsonAlias("death_year") LocalDate anoFalecimento){
+
 }
