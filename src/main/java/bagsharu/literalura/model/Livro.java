4a773247d3b79912f5381b5af31f8c731ce1e1f0
bagsharu <gabs.haru@gmail.com>
2024-12-10 17:49:33 -0300

    Classe Livro para receber as infos

---
+++ src/main/java/bagsharu/literalura/model/Livro.java
@@ -0,0 +1,17 @@
+package bagsharu.literalura.model;
+
+public class Livro {
+
+    private Integer count;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "count=" + count +
+                '}';
+    }
+}
