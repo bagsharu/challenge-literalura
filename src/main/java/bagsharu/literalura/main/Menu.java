8068d875689fb59d3625359c22e3156762539ef3
bagsharu <gabs.haru@gmail.com>
2024-12-11 15:37:28 -0300

    Método de Buscar Livro

--- src/main/java/bagsharu/literalura/main/Menu.java
+++ src/main/java/bagsharu/literalura/main/Menu.java
@@ -1,6 +1,8 @@
 package bagsharu.literalura.main;
 
+import bagsharu.literalura.model.LivroInfo;
 import bagsharu.literalura.service.ConsultaAPI;
+import bagsharu.literalura.service.ConverteDados;
 
 public class Menu {
 
@@ -16,5 +18,17 @@ public class Menu {
                 
                 """;
         System.out.println(TEXTO_MENU);
+    }
+
+    // Retorna um Record com as informações do Gutendex
+    public LivroInfo buscarLivro(String livroBusca) {
+
+        // Armazena as informações do json na variável
+        var json = consultas.requestData(livroBusca);
+
+        // Converte dados do json para Record
+        var convert = new ConverteDados();
+        return convert.converterDados(json,LivroInfo.class);
+
     }
 }
