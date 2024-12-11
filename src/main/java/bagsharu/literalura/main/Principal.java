71f5d04275149e0ec0409bb0a9b7939b4f6d3782
bagsharu <gabs.haru@gmail.com>
2024-12-11 13:36:53 -0300

    Classe principal

--- src/main/java/bagsharu/literalura/main/Principal.java
+++ src/main/java/bagsharu/literalura/main/Principal.java
@@ -1,4 +1,35 @@
 package bagsharu.literalura.main;
 
+import java.util.Scanner;
+
 public class Principal {
+
+    private final String URL_BASE = "https://gutendex.com/books";
+
+    private Scanner scanner = new Scanner(System.in);
+    private Menu controleMenu = new Menu();
+
+
+    public void IniciarMenu() {
+        int control = -1;
+
+        while (control != 0) {
+
+            controleMenu.ImprimeMenu();
+            control = scanner.nextInt();
+
+            switch (control) {
+                case (1):
+                    System.out.println("Teste");
+                    break;
+                case (0):
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Operação inválida.");
+            }
+        }
+
+    }
+
 }
