3c3b7068673eeba91e283f77d17a61e5f2a90a28
bagsharu <gabs.haru@gmail.com>
2024-12-11 16:37:50 -0300

    Renomear classe

--- src/main/java/bagsharu/literalura/model/Gutendex.java
+++ src/main/java/bagsharu/literalura/model/Gutendex.java
@@ -6,5 +6,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Resultado (@JsonAlias("results")List<LivroInfo> livrosBusca){
+public record Gutendex(@JsonAlias("results")List<LivroInfo> livrosBusca){
 }
