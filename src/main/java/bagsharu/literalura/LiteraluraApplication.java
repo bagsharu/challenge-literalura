1d48ab815be94da879c4ce86cffdcc4e55d656b0
bagsharu <gabs.haru@gmail.com>
2024-12-11 13:48:05 -0300

    Variável Spring Autowired

--- src/main/java/bagsharu/literalura/LiteraluraApplication.java
+++ src/main/java/bagsharu/literalura/LiteraluraApplication.java
@@ -1,12 +1,17 @@
 package bagsharu.literalura;
 
 import bagsharu.literalura.main.Principal;
+import bagsharu.literalura.repository.LivrosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private LivrosRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
