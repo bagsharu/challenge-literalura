ab08de0536eac62477512c744a8e0917a0057907
bagsharu <gabs.haru@gmail.com>
2024-12-11 13:37:19 -0300

    Rodando aplicação

--- src/main/java/bagsharu/literalura/LiteraluraApplication.java
+++ src/main/java/bagsharu/literalura/LiteraluraApplication.java
@@ -1,5 +1,6 @@
 package bagsharu.literalura;
 
+import bagsharu.literalura.main.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,5 +15,9 @@ public class LiteraluraApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World!");
+
+		Principal principal = new Principal();
+
+		principal.IniciarMenu();
 	}
 }
