dd3cbcf887c790c66d54b3bcc6004b00f05397e2
bielv <gabs.haru@gmail.com>
2024-12-17 23:27:12 -0300

    Método Query para consulta de título

--- src/main/java/bagsharu/literalura/repository/LivrosRepository.java
+++ src/main/java/bagsharu/literalura/repository/LivrosRepository.java
@@ -3,9 +3,12 @@ package bagsharu.literalura.repository;
 import bagsharu.literalura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LivrosRepository extends JpaRepository<Livro, Long> {
 
     Optional<Livro> findByTituloLivroContains(String titulo);
+
+    List<Livro> findByLinguagemLivroContains(String idioma);
 }
