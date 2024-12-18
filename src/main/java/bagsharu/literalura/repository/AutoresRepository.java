e4d435a17e44ed26ec1d5a8d55ed62d6a757e4b3
bielv <gabs.haru@gmail.com>
2024-12-17 23:15:00 -0300

    Método Query para buscar autores vivos em um determinado ano

--- src/main/java/bagsharu/literalura/repository/AutoresRepository.java
+++ src/main/java/bagsharu/literalura/repository/AutoresRepository.java
@@ -2,10 +2,16 @@ package bagsharu.literalura.repository;
 
 import bagsharu.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AutoresRepository extends JpaRepository<Autor, Long> {
 
     Optional<Autor> findByNomeAutorContains(String nome);
+
+    @Query("SELECT a FROM Autor a WHERE a.anoNascimento >= %:ano% AND a.anoFalecimento <= %:ano%")
+    List<Autor> buscarAno(@Param("ano") Integer ano);
 }
