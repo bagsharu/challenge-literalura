d5e86d24bce26a7a9946d0f3522f37e35ba95335
bielv <gabs.haru@gmail.com>
2024-12-17 22:28:41 -0300

    Método Query para consultar dados

--- src/main/java/bagsharu/literalura/repository/LivrosRepository.java
+++ src/main/java/bagsharu/literalura/repository/LivrosRepository.java
@@ -3,5 +3,9 @@ package bagsharu.literalura.repository;
 import bagsharu.literalura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LivrosRepository extends JpaRepository<Livro, Long> {
+
+    Optional<Livro> findByTituloContains(String titulo);
 }
